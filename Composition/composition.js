2808e23839513a399f3cae52f7554aae90061743
thallesManjaterra <thalles.manjaterra@gmail.com>
2020-05-24 00:40:49 -0300

    Composition

---
+++ Composition/composition.js
@@ -0,0 +1,20 @@
+const { compose } = require('ramda');
+
+// const _compose = (...fns) => (x) => fns.reduceRight((r, fn) => fn(r), x);
+
+const add = (x, y) => x + y;
+
+const toUpper = str => str.toUpperCase();
+
+const exclaim = str => str + '!';
+
+const  first = xs => xs[0];
+
+// const compose = (f, g) => (x) => f(g(x));
+// const compose = x => (exclaim(toUpper(x));
+
+const loudFirst = compose(toUpper, first);
+
+const shout = compose(exclaim, loudFirst);
+
+console.log(shout('tears'));
