9d6d58a3540b62531a3d425003835104734887f0
thallesManjaterra <thalles.manjaterra@gmail.com>
2020-05-23 23:06:48 -0300

    currying - replace vowels

--- Currying/curry.js
+++ Currying/curry.js
@@ -1,4 +1,6 @@
-const curry = (f) => (x) => (y) => f(x, y);
+const { curry } = require('ramda');
+
+//const curry = (f) => (x) => (y) => f(x, y);
 
 const modulo = curry((x, y) => y % x);
 
@@ -12,3 +14,10 @@ const result = getOdds([1, 2, 3, 4, 5]);
 
 console.log(result);
 
+const replace = curry((regExp, replacement, str) => str.replace(regExp, replacement))
+
+const replaceVowels = replace(/[AEIOU]/ig, '!');
+
+const r = replaceVowels('Hey I have some words');
+
+console.log(r);
