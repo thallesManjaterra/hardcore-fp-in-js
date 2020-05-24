7a3334cfe53920781032d30289a9a0d30918793d
thallesManjaterra <thalles.manjaterra@gmail.com>
2020-05-23 23:05:48 -0300

    currying - getOdds

--- Currying/curry.js
+++ Currying/curry.js
@@ -1,11 +1,14 @@
-const add = (x, y) => x + y;
-
 const curry = (f) => (x) => (y) => f(x, y);
 
 const modulo = curry((x, y) => y % x);
 
 const isOdd = modulo(2);
 
-const result = isOdd(3);
+const filter = curry((f, xs) => xs.filter(f));
+
+const getOdds = filter(isOdd);
+
+const result = getOdds([1, 2, 3, 4, 5]);
 
 console.log(result);
+
